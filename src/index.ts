097feca49f2baac09c4941ccf8c4496e3414b054
SRT DEV <sahirruiz4@gmail.com>
2025-06-16 16:56:08 -0500

    feat: agregar manejo de puerto por defecto en el servidor y asegurar la ejecución de seeders

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,18 @@ import "dotenv/config";
 import { startServer } from "@infrastructure/http/server";
 import { connectDB } from "@infrastructure/persistence/database/mongodb/connection";
 import { seedMembershipsIfEmpty } from "./infrastructure/database/seeds/membershipSeeder";
+import { seedAdminsIfEmpty } from "./infrastructure/database/seeds/adminSeeder";
 
 async function bootstrap() {
   try {
     await connectDB();
 
+    // Ejecutar seeders si es necesario
     await seedMembershipsIfEmpty();
+    await seedAdminsIfEmpty();
 
     const server = await startServer();
-    console.log(`Server running on port ${process.env.PORT}`);
+    console.log(`Server running on port ${process.env.PORT || 3000}`);
   } catch (error) {
     console.error("Error starting server:", error);
     process.exit(1);
