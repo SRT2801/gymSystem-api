9d6358dc1207060cad921fd897440a65d48be8b2
SRT DEV <sahirruiz4@gmail.com>
2025-06-16 03:19:58 -0500

    feat: agregar funciones para conectar y desconectar de MongoDB

---
+++ src/infrastructure/persistence/database/mongodb/connection.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export async function connectDB(): Promise<void> {
+  try {
+  
+    mongoose.set('strictQuery', false);
+    
+    const mongoUri =
+      process.env.MONGODB_URI || "mongodb://localhost:27017/gymSystem";
+    await mongoose.connect(mongoUri);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+}
+
+export async function disconnectDB(): Promise<void> {
+  await mongoose.disconnect();
+  console.log("MongoDB disconnected");
+}
