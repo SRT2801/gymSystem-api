6287b6fe83b5d3d01b7780fe210b1920eb7ff06d
SRT DEV <sahirruiz4@gmail.com>
2025-06-16 17:00:43 -0500

    feat: reorganizar el servidor para gestionar rutas y errores de manera más clara

--- src/infrastructure/http/server.ts
+++ src/infrastructure/http/server.ts
@@ -1,29 +1,39 @@
 import express, { Application } from "express";
 import cors from "cors";
-// Eliminamos la importación del connectDB ya que ahora se gestiona en index.ts
 import { membersRouter } from "./routes/members.routes";
 import { membershipsRouter } from "./routes/memberships.routes";
 import { subscriptionsRouter } from "./routes/subscriptions.routes";
+import { authRouter } from "./routes/auth.routes";
+import { errorHandler } from "./middlewares/errorHandler";
 
 export async function startServer(): Promise<Application> {
   const app: Application = express();
   const port = process.env.PORT || 3000;
 
-
   app.use(cors());
   app.use(express.json());
 
-
+  // Rutas
+  app.use("/api/auth", authRouter);
   app.use("/api/members", membersRouter);
   app.use("/api/memberships", membershipsRouter);
   app.use("/api/subscriptions", subscriptionsRouter);
 
-  
   app.get("/health", (req, res) => {
     res.status(200).send("OK");
   });
 
- 
+
+  app.use((req, res) => {
+    res.status(404).json({
+      status: "error",
+      message: `La ruta ${req.originalUrl} no existe en este servidor`,
+    });
+  });
+
+  
+  app.use(errorHandler);
+
   app.listen(port);
 
   return app;
